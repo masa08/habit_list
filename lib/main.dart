1cff45d8032691223965e089edde6be3fe012f68
masa08 <ushimasarug08@gmail.com>
2022-05-03 13:58:28 +0900

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_list/app.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
-  // TODO: initialize
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
-      child: App())
-    );
+      child: App()
+    )
+  );
 }
