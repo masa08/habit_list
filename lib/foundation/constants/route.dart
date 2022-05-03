e3bd8ebc44f7851b4118f7b735fc444d2637f5a0
masa08 <ushimasarug08@gmail.com>
2022-05-03 16:09:34 +0900

    init firebase auth

--- lib/foundation/constants/route.dart
+++ lib/foundation/constants/route.dart
@@ -1,10 +1,12 @@
-// used for page navigation
+// used for page navigation in go router
 class RoutePath {
   static const home = "/";
+  static const singIn = "/singIn";
   static const add = "/add";
 }
 
 // used for router define
 class RouteName {
+  static const singIn = "singIn";
   static const add = "add";
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:habit_list/foundation/constants/route.dart';
 import 'package:habit_list/pages/add_habit_page.dart';
 import 'package:habit_list/pages/main_page.dart';
+import 'package:habit_list/pages/sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final routerProvider = Provider(
-  (ref) => GoRouter(
-    initialLocation: RoutePath.home,
+final routerProvider = Provider((ref) {
+  final user = FirebaseAuth.instance.currentUser;
+
+  return GoRouter(
+    initialLocation: user != null ? RoutePath.home : RoutePath.singIn,
     routes: [
       GoRoute(
         path: RoutePath.home,
@@ -17,6 +21,11 @@ final routerProvider = Provider(
             name: RouteName.add,
             path: RouteName.add,
             builder: (context, state) => const AddHabitPage(),
+          ),
+          GoRoute(
+            name: RouteName.singIn,
+            path: RouteName.singIn,
+            builder: (context, state) => const SignInPage(),
           ),
         ],
       ),
@@ -29,5 +38,5 @@ final routerProvider = Provider(
         ),
       ),
     ),
-  ),
-);
+  );
+});
